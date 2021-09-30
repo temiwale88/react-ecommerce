440ba8ea88517eb149682b15ba9ae3eee66ba22e
Elijah Adeoye <Temiwale88@gmail.com>
2021-09-30 08:14:28 -0700

    added testing to with Profiler to HomePage

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {Profiler} from 'react';
 import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 
 const HomePage = () => (
     <div className="homepage">
-        <Directory />
+        {/* Profiler is for testing how long this runs for. See lecture 288: React <Profiler> */}
+        <Profiler id="Directory" onRender={(id, phase, actualDuration) => console.log({id, phase, actualDuration}) }>
+            <Directory />
+        </Profiler>
     </div>
 );
 
